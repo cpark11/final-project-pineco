feaf4c5cba002def799da1cd1c272056f9ba0af2
Candace Park <candace.park11@gmail.com>
2016-11-07 23:02:23 -0500

    camera icon

--- app/src/main/java/com/pineco/flickrtron/MainActivity.java
+++ app/src/main/java/com/pineco/flickrtron/MainActivity.java
@@ -41,6 +41,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Open Camera", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         // Restore preferences
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         String editTextValue = settings.getString("editTextValue", "none");
