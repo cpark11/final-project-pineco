50c9bc86e94890ec8b10b1428951e72168d2ff42
Candace Park <candace.park11@gmail.com>
2016-11-07 22:38:02 -0500

    shared prefs working

--- app/src/main/java/com/pineco/flickrtron/MainActivity.java
+++ app/src/main/java/com/pineco/flickrtron/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pineco.flickrtron;
 
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -28,10 +29,20 @@ public class MainActivity extends AppCompatActivity {
 
     static final String API_KEY = "379c73dfd6eede56394f7dc6ab60921a";
     static final String API_URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search";
+
+    public static final String PREFS_NAME = "PrefsFile";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        String editTextValue = settings.getString("editTextValue", "none");
+
+        EditText pastSearch = (EditText)findViewById(R.id.tag);
+        pastSearch.setText(editTextValue);
+
         responseView = (TextView) findViewById(R.id.responseView);
         tag = (EditText) findViewById(R.id.tag);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -125,6 +136,22 @@ public class MainActivity extends AppCompatActivity {
 //                e.printStackTrace();
 //            }
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Using Preferences
+        // We need an Editor object to make preference changes.
+        // All objects are from android.context.Context
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        EditText editText = (EditText) findViewById(R.id.tag);
+        editor.putString("editTextValue", editText.getText().toString());
+
+        // Commit the edits!
+        editor.commit();
     }
 
 }
